0ace314feebcd0e9ea51b43fe261772d9f512a0e
Paulo Vieira <45016641+pcvp@users.noreply.github.com>
2022-08-23 14:19:00 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,15 @@ import Layout from "./../components/layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+       <Head>
+         <title>Trinks rollbacks</title>
+         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+       </Head>
+       <Layout>
+         <Component {...pageProps} />
+       </Layout>
+    </>
   );
 }
 export default MyApp;
